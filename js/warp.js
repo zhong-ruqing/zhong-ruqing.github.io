b75817575a094a911d72d19e33c4c023f239cc49
zhongruqing <zrq.zhongruqing@gmail.com>
2020-07-14 16:43:55 +0200

    Site updated: 2020-07-14 16:43:51

--- js/warp.js
+++ js/warp.js
@@ -1,6 +1,9 @@
-$("img").not('.svg symbol').each(function () {
+/*Add fancybox function, and not apply fancybox on logo, if the image has class "nofancybox", then it cannot be open with fancybox*/
+$("img").not('.logo img').each(function () {
    // $(this).attr("data-fancybox", "gallery"); 直接给img添加data-fancybox会导致点击图片后图片消失
     var element = document.createElement("a");
+    var $image = $(this);
+    if ($(this).hasClass('nofancybox')) return;
     $(element).attr("data-fancybox", "gallery");
     $(element).attr("href", $(this).attr("src"));
     $(this).wrap(element);
